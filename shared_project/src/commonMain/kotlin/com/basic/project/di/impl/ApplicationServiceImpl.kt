1de4a829e25ad6bfa96c6632b886ce161df99710
范俊 <fj@yoc.tech>
2026-07-03 16:17:50 +0800

    去掉无异议代码

--- shared_project/src/commonMain/kotlin/com/basic/project/di/impl/ApplicationServiceImpl.kt
+++ shared_project/src/commonMain/kotlin/com/basic/project/di/impl/ApplicationServiceImpl.kt
@@ -1,15 +1,9 @@
 package com.basic.project.di.impl
 
 import com.basic.base.di.service.ApplicationService
-import com.basic.base.ktx.applicationScope
-import com.basic.base.ktx.launchScope
-import com.basic.base.webview.platform.preloadWebkit
 
 class ApplicationServiceImpl : ApplicationService {
     override fun onCreate() {
-        applicationScope.launchScope {
-            preloadWebkit("https://baidu.com")
-        }
     }
 
     override fun onBackground() {
