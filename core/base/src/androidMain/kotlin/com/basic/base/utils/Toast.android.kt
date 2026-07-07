f0bea870b48118aeb7402d295c50eea5ad8dcf23
范俊 <xxxx@yoc.tech>
2026-07-07 16:17:16 +0800

    回退android-nativeToast实现

--- core/base/src/androidMain/kotlin/com/basic/base/utils/Toast.android.kt
+++ core/base/src/androidMain/kotlin/com/basic/base/utils/Toast.android.kt
@@ -1,30 +1,12 @@
 package com.basic.base.utils
 
 import android.widget.Toast
-import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.FragmentActivity
 import com.basic.base.ktx.applicationScope
 import com.basic.base.ktx.launchScope
 import com.basic.base.local.UIContainer
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-
-/**
- * 查找最顶层的正在显示的DialogFragment
- */
-internal fun FragmentActivity.findTopDialogFragmentByWindow(): DialogFragment? {
-    val fragments = supportFragmentManager.fragments
-    for (i in fragments.indices.reversed()) {
-        val fragment = fragments[i]
-        if (fragment is DialogFragment) {
-            if (fragment.isAdded && fragment.isVisible && fragment.dialog?.isShowing == true) {
-                return fragment
-            }
-        }
-    }
-    return null
-}
 
 actual fun nativeToast(
     uiContainer: UIContainer,
@@ -39,23 +21,8 @@ actual fun nativeToast(
                 Snackbar.LENGTH_SHORT
             }
             ActivityStackManager.getCurrentActivity()?.let {
-                if (it is FragmentActivity) {
-                    val hasDialogFragment = (uiContainer as? FragmentActivity)?.findTopDialogFragmentByWindow()
-                    withContext(Dispatchers.Main) {
-                        if (hasDialogFragment == null) {
-                            Snackbar.make(
-                                uiContainer.findViewById(android.R.id.content), msg, length
-                            ).show()
-                        } else {
-                            hasDialogFragment.dialog?.window?.decorView?.let {
-                                Snackbar.make(it, msg, length).show()
-                            }
-                        }
-                    }
-                } else {
-                    withContext(Dispatchers.Main) {
-                        Toast.makeText(it, text, length).show()
-                    }
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(it, text, length).show()
                 }
             }
         }
