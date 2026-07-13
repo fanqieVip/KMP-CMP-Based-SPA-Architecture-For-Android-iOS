7fcf23f372878256f92efdd25c5f70a4b5020d08
范俊 <fj@yoc.tech>
2026-07-13 18:42:04 +0800

    修改nativeToast线程

--- core/base/src/iosMain/kotlin/com/basic/base/utils/Toast.ios.kt
+++ core/base/src/iosMain/kotlin/com/basic/base/utils/Toast.ios.kt
@@ -4,20 +4,25 @@ package com.basic.base.utils
 
 import cocoapods.Toast.CSToastPositionBottom
 import cocoapods.Toast.makeToast
+import com.basic.base.ktx.applicationScope
+import com.basic.base.ktx.launchScope
 import com.basic.base.local.UIContainer
 import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.coroutines.Dispatchers
 
 actual fun nativeToast(
     uiContainer: UIContainer,
     text: String?,
     @NativeToastDuration duration: Int
 ) {
-    text?.let { text ->
-        val length = if (duration == Toast_Duration_Long) {
-            5.0
-        } else {
-            3.0
+    applicationScope.launchScope(Dispatchers.Main) {
+        text?.let { text ->
+            val length = if (duration == Toast_Duration_Long) {
+                5.0
+            } else {
+                3.0
+            }
+            uiContainer.view.makeToast(text, length, CSToastPositionBottom)
         }
-        uiContainer.view.makeToast(text, length, CSToastPositionBottom)
     }
 }
