dbf86ae700d700278a58a0f0dc2f0bac5a5673e3
范俊 <xxxx@yoc.tech>
2026-07-15 12:53:38 +0800

    修复h5输入框引发的ios崩溃

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,23 +1,52 @@
 import SwiftUI
 import ComposeApp
+import UIKit
 
 // SceneDelegate，用于接收 Universal Link 和 URL Scheme 回调
 class SwiftSceneDelegate: NSObject, UIWindowSceneDelegate {
-    
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        SwiftAppDelegate.syncKeyWindow()
+    }
+
+    // 处理 Universal Link (如微信 SDK 要求的地方)
     func scene(_ scene: UIScene, continue userActivity: NSUserActivity) {
         AppDelegate.shared.sceneContinueUserActivity(userActivity: userActivity)
     }
-    
+
+    // 处理 URL Scheme (如微信、支付宝支付返回)
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
         AppDelegate.shared.sceneOpenURLContexts(urlContexts: URLContexts)
     }
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        SwiftAppDelegate.syncKeyWindow()
         AppDelegate.shared.sceneWillConnectToOptions(userActivities: connectionOptions.userActivities, urlContexts: connectionOptions.urlContexts)
     }
 }
 
 class SwiftAppDelegate: NSObject, UIApplicationDelegate {
+    private weak var currentWindow: UIWindow?
+
+    @objc dynamic var window: UIWindow? {
+        get {
+            currentWindow ?? SwiftAppDelegate.findKeyWindow()
+        }
+        set {
+            currentWindow = newValue
+        }
+    }
+
+    static func syncKeyWindow() {
+        (UIApplication.shared.delegate as? SwiftAppDelegate)?.window = findKeyWindow()
+    }
+
+    private static func findKeyWindow() -> UIWindow? {
+        UIApplication.shared.connectedScenes
+        .compactMap { $0 as? UIWindowScene }
+        .flatMap { $0.windows }
+        .first { $0.isKeyWindow }
+    }
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         AppDelegate.shared.onAppCreate()
         UIViewController.swizzleAllLifecycle()
