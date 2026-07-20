cd998e5ecb0597bc981a608de3e85d0d0f142a89
范俊 <fj@yoc.tech>
2026-07-20 10:21:11 +0800

    优化初始化流程

--- core/common/src/androidMain/kotlin/com/basic/common/Application.android.kt
+++ core/common/src/androidMain/kotlin/com/basic/common/Application.android.kt
@@ -1,10 +1,28 @@
 package com.basic.common
 
-import com.basic.common.utils.ApkEnvVerifyTask
+import com.basic.base.ktx.applicationScope
+import com.basic.base.ktx.launchScope
+import com.basic.native.checkEnv
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 
 internal actual object Application {
     actual fun onCreate() {
         //apk运行环境自检
-        ApkEnvVerifyTask.start()
+        autoVerifyEnv()
+    }
+}
+
+/**
+ * apk完整性校验
+ * 1.所有代码均在代码块中，不要抽取方法，会制造代码安全漏洞
+ * 2.封装TimerTask是为了便于vmp加密
+ */
+private fun autoVerifyEnv() {
+    applicationScope.launchScope {
+        while (isActive){
+            checkEnv()
+            delay(60000)
+        }
     }
 }
--- core/common/src/commonMain/kotlin/com/basic/common/di/impl/ApplicationServiceImpl.kt
+++ core/common/src/commonMain/kotlin/com/basic/common/di/impl/ApplicationServiceImpl.kt
@@ -1,6 +1,9 @@
 package com.basic.common.di.impl
 
 import com.basic.base.di.service.ApplicationService
+import com.basic.base.ktx.applicationScope
+import com.basic.base.ktx.launchScope
+import com.basic.base.webview.platform.preloadWebkit
 import com.basic.common.Application
 
 /**
@@ -9,6 +12,8 @@ import com.basic.common.Application
 class ApplicationServiceImpl : ApplicationService {
     override fun onCreate() {
         Application.onCreate()
+        //预初始化webkit
+        applicationScope.launchScope { preloadWebkit() }
     }
 
     override fun onBackground() {
