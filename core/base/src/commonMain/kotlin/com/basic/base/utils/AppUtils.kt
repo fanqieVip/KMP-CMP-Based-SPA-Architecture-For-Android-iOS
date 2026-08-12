c1333711c0809e29e5aadeb411466ebc2470614e
范俊 <fj@yoc.tech>
2026-08-12 15:28:00 +0800

    AppUtils新增检测定位硬件是否已开启方法

--- core/base/src/commonMain/kotlin/com/basic/base/utils/AppUtils.kt
+++ core/base/src/commonMain/kotlin/com/basic/base/utils/AppUtils.kt
@@ -4,17 +4,23 @@ import com.basic.base.local.UIContainer
 import io.github.vinceglb.filekit.PlatformFile
 
 /**
- * @Description:    app工具类
- * @Author:         fanj
- * @CreateDate:     2026/7/15 11:39
- * @Version:
+ * @Description:    App 系统工具能力集合。
+ * @Author:         范俊
+ * @CreateDate:     2026/08/12 15:00
  */
 expect object AppUtils {
     /**
-     * 安装安卓apk
+     * 查询系统定位服务是否已开启，不检查应用定位权限。
      *
-     * @param path apk文件
-     * @param uiContainer 宿主
+     * @return 系统定位服务开关已开启时返回 true，否则返回 false。
+     */
+    fun isLocationServiceEnabled(): Boolean
+
+    /**
+     * 安装 Android APK 文件。
+     *
+     * @param uiContainer 页面宿主。
+     * @param path APK 文件。
      */
     fun installApk(uiContainer: UIContainer, path: PlatformFile)
 }
--- core/base/src/iosMain/kotlin/com/basic/base/utils/AppUtils.ios.kt
+++ core/base/src/iosMain/kotlin/com/basic/base/utils/AppUtils.ios.kt
@@ -2,8 +2,11 @@ package com.basic.base.utils
 
 import com.basic.base.local.UIContainer
 import io.github.vinceglb.filekit.PlatformFile
+import platform.CoreLocation.CLLocationManager
 
 actual object AppUtils {
+    actual fun isLocationServiceEnabled(): Boolean = CLLocationManager.locationServicesEnabled()
+
     actual fun installApk(uiContainer: UIContainer, path: PlatformFile) {
     }
 }
